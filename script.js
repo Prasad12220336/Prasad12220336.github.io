f64b3228d1856c5cdeb13b848c35ad85db776cb8
Venkata Durga Prasad <121507816+Prasad12220336@users.noreply.github.com>
2024-09-28 10:28:07 +0530

    script.js

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-slide");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.transform = `translateX(${-(slideIndex - 1) * 100}%)`;
+    }
+
+    // Remove "active" class from all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Add "active" class to the current dot
+    dots[slideIndex - 1].className += " active";
+}
+
