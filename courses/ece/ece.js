03b49790dac17dc8289d1268b8007e4ecb5819fd
Venkata Durga Prasad <121507816+Prasad12220336@users.noreply.github.com>
2024-10-02 18:11:13 +0530

    ece.js

--- courses/ece/ece.js
+++ courses/ece/ece.js
@@ -1,1 +1,8 @@
-
+function toggleResources(id) {
+    var resources = document.getElementById(id);
+    if (resources.style.display === "block") {
+        resources.style.display = "none";
+    } else {
+        resources.style.display = "block";
+    }
+}
